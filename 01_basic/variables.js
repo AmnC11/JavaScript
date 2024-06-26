8a2427e1cacc0276f12721a406bc75ded0aee362
Aman <Amanchy4982@gmail.com>
2024-06-26 07:02:16 +0000

    Variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,8 @@
+const AccountNumber= 12357
+console.log(AccountNumber)
+let AccountEmail="AManchy4892@gmail.com"
+let pass= "123456987"
+console.table([AccountNumber,AccountEmail,pass])
+/*
+we don't use var very much due tu its peoblem.
+*/
