34b9ec634a3e932f911e862f936a3e367f449fe1
Aman <Amanchy4982@gmail.com>
2024-06-26 07:01:47 +0000

    data types

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all the code as newer version 
+// alert(3+2) // we cant use alert because we are using node instead of web browser
+console.log(3
+    +3) // this is bad practise. code readablility is not good
+
+console.log("Aman")
+let name= "Aman"
+let age= 18
+let isLoggedIn= true
+console.table([name,age, isLoggedIn])
+
+// number => 2 tothe power 53
+// BigInt
+// string=> ""
+//  Boolean>= true/false
+//  null => standalone 
+//  undefined =>
+//     symbol >= unique
+console.log(typeof isLoggedIn)
